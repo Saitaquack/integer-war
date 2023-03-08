3dd4ae699d9080f6323581de9ad6d4371667ba51
MaxVo-spec <voltzmaxime@gmail.com>
2023-03-08 20:00:38 +0100

    Added chat !

--- public/js/battleComments.js
+++ public/js/battleComments.js
@@ -6,71 +6,73 @@ function updateBattleComment(message)
 }
 
 socket.on('warscore', function(score) {
-    if(score >= 50000)
+
+    if(score > 0)
     {
-        updateBattleComment('RIP negatives 💀');
-    }
-    else if(score >= 25000)
-    {
-        updateBattleComment('Positive gang HYPE !!');
-    }
-    else if(score >= 10000)
-    {
-        updateBattleComment('Positives are dominating negatives !');
-    }
-    else if(score >= 2500)
-    {
-        updateBattleComment('Negative Gang, what are you guys doing ?!');
-    }
-    else if(score >= 1000)
-    {
-        updateBattleComment('Positives are getting far, better watch out !');
-    }
-    else if(score >= 250)
-    {
-        updateBattleComment('Positives are keeping the lead !');
-    }
-    else if(score >= 1)
-    {
-        updateBattleComment('Seems like positives took the lead !');
+        if(score >= 50000)
+        {
+            updateBattleComment('RIP negatives 💀');
+        }
+        else if(score >= 25000)
+        {
+            updateBattleComment('Positive gang HYPE !!');
+        }
+        else if(score >= 10000)
+        {
+            updateBattleComment('Positives are dominating negatives !');
+        }
+        else if(score >= 2500)
+        {
+            updateBattleComment('Negative Gang, what are you guys doing ?!');
+        }
+        else if(score >= 1000)
+        {
+            updateBattleComment('Positives are getting far, better watch out !');
+        }
+        else if(score >= 250)
+        {
+            updateBattleComment('Positives are keeping the lead !');
+        }
+        else if(score >= 1)
+        {
+            updateBattleComment('Seems like positives took the lead !');
+        }
     }
     else if(score == 0)
     {
         updateBattleComment('Perfect balance, as everything should be.');
     }
-    else if(score <= -1)
-    {
-        updateBattleComment('Seems like negatives took the lead !');
-    }
-    else if(score <= -250)
-    {
-        updateBattleComment('Negatives are keeping the lead !');
-    }
-    else if(score <= -1000)
-    {
-        updateBattleComment('Negatives are getting far, better watch out !');
-    }
-    else if(score <= -2500)
-    {
-        updateBattleComment('Positive Gang, what are you guys doing ?!');
-    }
-    else if(score <= -10000)
-    {
-        updateBattleComment('Negatives are dominating positives !');
-    }
-    else if(score <= -25000)
-    {
-        updateBattleComment('Negative gang HYPE !!');
-    }
-    else if(score <= -50000)
-    {
-        updateBattleComment('RIP positives 💀');
-    }
-
     else
     {
-        updateBattleComment('');
-    }    
+        if(score <= -50000)
+        {
+            updateBattleComment('RIP positives 💀');
+        }
+        else if(score <= -25000)
+        {
+            updateBattleComment('Negative gang HYPE !!');
+        }
+        else if(score <= -10000)
+        {
+            updateBattleComment('Negatives are dominating positives !');
+        }
+        else if(score <= -2500)
+        {
+            updateBattleComment('Positive Gang, what are you guys doing ?!');
+        }
+        else if(score <= -1000)
+        {
+            updateBattleComment('Negatives are getting far, better watch out !');
+        }
+        else if(score <= -250)
+        {
+            updateBattleComment('Negatives are keeping the lead !');
+        }
+        else if(score <= -1)
+        {
+            updateBattleComment('Seems like negatives took the lead !');
+        }
+    }   
 });
 
 
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,18 +1,52 @@
 let theChat = document.getElementById("chat");
-let sendMessage = document.getElementById("send-chat");
+let sendChat = document.getElementById("send-chat");
 let chatInput = document.getElementById("chat-input");
+let chatHide = document.getElementById("hide-chat");
 
-sendMessage.addEventListener("click", function(){
+let maxChatLength = 300;
+
+function sendMessage(){
     let message = chatInput.value;
     if(message.length > 0)
     {
-        console.log("Oui !")
-        socket.emit("sendchat", {
-            message: message
+        socket.emit('sendchat', {
+            content: message
         })
     }
-})
+    chatInput.value = '';
+    chatInput.focus();
+}
+
+sendChat.addEventListener("click", function(){
+    sendMessage();
+});
+
+chatHide.addEventListener("click", function(){
+    theChat.classList.toggle("hidden");
+    sendChat.classList.toggle("hidden");
+    chatInput.classList.toggle("hidden");
+
+    if(theChat.classList.contains("hidden")){
+        chatHide.innerText = "Show Chat";
+    }
+    else {
+        chatHide.innerText = "Hide Chat";
+    }
+});
+
+chatInput.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      sendMessage();
+    }
+  });
 
 socket.on('updatechat', function(message){
-
+    let p = document.createElement('p');
+    p.innerText = message.content;
+    if(p.innerText.length > maxChatLength)
+    {
+        p.innerText = message.content.substring(0, maxChatLength);
+    }
+    theChat.append(p);
 });
